47b4bfe1c82fedec8bf082a1d5613ed7d2db8e8a
onionc <wsrfssx@yeah.net>
2020-04-05 22:38:31 +0800

    fix [QrCode]: 增加了输出格式

--- QrCode/ErrorCorrectionCoding.java
+++ QrCode/ErrorCorrectionCoding.java
@@ -6,5 +6,10 @@ public class ErrorCorrectionCoding{
     public static void main(String[] args){
         // System.out.println("纠错算法");
         Polynomial a = new Polynomial();
+        a.addItem(90,3);       
+        a.addItem(-1,-2);
+        a.addItem(5,1);
+
+        System.out.println(a);
     }
 }
--- QrCode/Polynomial.java
+++ QrCode/Polynomial.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * 一元多项式
@@ -14,23 +16,41 @@ import java.util.List;
 class Node{
     public int coef; // 系数
     public int expn; // 指数
-    private final static char x = 'x'; // 未知数表示
+    private final static char x = 'x'; // 未知数的表示符号
+    private enum FORMAT {NORMAL, LATEX}; // 打印格式
+    private FORMAT f = FORMAT.NORMAL; // 默认的打印格式
     
     public Node(int coef, int expn){
         this.coef = coef;
         this.expn = expn;
     }
     
+    /**
+     * 设置输出格式
+     * @param fi 1:普通格式 (+90x^3-1x^-2+5x^1)，2 latex (+90x^{3}-1x^{-2}+5x^{1})
+     */
+    public void setFormat(int fi){
+        switch(fi){
+            case 1:
+                this.f = FORMAT.NORMAL; 
+                break;
+            case 2:
+                this.f = FORMAT.LATEX;
+                break;
+        }
+    }
+
     public String toString(){
-        return String.format("%dx%d", coef, expn);
+        //return String.format("%+d%s^%d", coef, x, expn);
+        return String.format("%1$+d%3$s^{%2$d}", coef, expn, x);
     }
 }
 public class Polynomial {
     private List<Node> poly;
-    private Iterator<Node> iterator;
+    private Iterator<Node> iter;
 
     public Polynomial(){
-        this.poly = new ArrayList<Node>();
+        this.poly = new LinkedList<Node>();
         this.reset();
     }
 
@@ -38,13 +58,13 @@ public class Polynomial {
      * 重置迭代器
      */
     private void reset(){
-        this.iterator = poly.iterator();
+        this.iter = poly.iterator();
     }
 
     /**
      * 添加一项
      */
-    public void add(int coef, int expn){
+    public void addItem(int coef, int expn){
         this.poly.add(new Node(coef, expn));
     }
 
@@ -52,10 +72,10 @@ public class Polynomial {
     public String toString(){
         this.reset();
         StringBuilder s = new StringBuilder();
-        while(this.iterator.hasNext()){
-            // todo
+        while(this.iter.hasNext()){
+            s.append(this.iter.next());
         }
-        return "";
+        return s.toString();
     }
 
 }
