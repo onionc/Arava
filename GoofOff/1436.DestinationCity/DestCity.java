64c21364b4bcb95a96347704b8d74d7c3a31a7cb
ChadJ <wsrfssx@yeah.net>
2020-05-14 00:11:20 +0800

    [LeetCode] fix 1436: 用数组存储，用时从7ms降到了4ms

--- GoofOff/1436.DestinationCity/DestCity.java
+++ GoofOff/1436.DestinationCity/DestCity.java
@@ -3,24 +3,53 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+// class Solution {
+//     public String destCity(List<List<String>> paths) {
+//         List<String> s1 = new ArrayList<>();
+//         List<String> s2 = new ArrayList<>();
+
+//         Iterator iter = paths.iterator();
+//         while(iter.hasNext()){
+//             List<String> a = (List<String>)iter.next();
+//             s2.add(a.get(0));
+//             s1.add(a.get(1));
+//         }
+        
+//         for(int i=0,j=0;i<s1.size();i++){
+//             String t = s1.get(i);
+//             for(j=0;j<s2.size();j++){
+//                 if(t.equals(s2.get(j))) break;
+//             }
+//             if(j==s2.size()){
+//                 return t;
+//             }
+//         }
+//         return "";
+//     }
+// }
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 class Solution {
     public String destCity(List<List<String>> paths) {
-        List<String> s1 = new ArrayList<>();
-        List<String> s2 = new ArrayList<>();
+        int len = paths.size();
+        String s1[] = new String[len];
+        String s2[] = new String[len];
 
-        Iterator iter = paths.iterator();
-        while(iter.hasNext()){
-            List<String> a = (List<String>)iter.next();
-            s2.add(a.get(0));
-            s1.add(a.get(1));
+        for(int i=0; i<paths.size();i++){
+            s2[i] = paths.get(i).get(0);
+            s1[i] = paths.get(i).get(1);
         }
-        
-        for(int i=0,j=0;i<s1.size();i++){
-            String t = s1.get(i);
-            for(j=0;j<s2.size();j++){
-                if(t.equals(s2.get(j))) break;
+  
+        for(int i=0,j=0;i<s1.length;i++){
+            String t = s1[i];
+            for(j=0;j<s2.length;j++){
+                if(t.equals(s2[j])) break;
             }
-            if(j==s2.size()){
+            if(j==s2.length){
                 return t;
             }
         }
